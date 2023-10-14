e550b1f97fa5cd929984d0b5d77c4c62a907814f
Norberto Monteiro de Melo <nrbrtmntr@gmail.com>
2023-10-14 20:06:45 -0300

    Angular Projeto - Store Cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ProductListComponent } from './product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ProductListComponent},
+  {path: 'products/:productId', component: ProductDetailsComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'shipping', component: ShippingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProductDetailsComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    TopBarComponent,
+    CartComponent,
+    ShippingComponent
+   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
